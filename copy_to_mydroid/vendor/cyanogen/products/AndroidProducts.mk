c558c4fd3bf5f3ece934b1e7a6853f00f2934d0a
Kenji Yosa <kyosa.dev@gmail.com>
2010-07-08 13:07:58 +0200

    modified build script

--- copy_to_mydroid/vendor/cyanogen/products/AndroidProducts.mk
+++ copy_to_mydroid/vendor/cyanogen/products/AndroidProducts.mk
@@ -1,5 +1,8 @@
 PRODUCT_MAKEFILES := \
+        $(LOCAL_DIR)/cyanogen_passion.mk \
+        $(LOCAL_DIR)/cyanogen_inc.mk \
         $(LOCAL_DIR)/cyanogen_hero.mk \
+        $(LOCAL_DIR)/cyanogen_sholes.mk \
 	$(LOCAL_DIR)/cyanogen_dream_sapphire.mk \
 	$(LOCAL_DIR)/cyanogen_bravo.mk \
 	$(LOCAL_DIR)/cyanogen_espresso.mk \
